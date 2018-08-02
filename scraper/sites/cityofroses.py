ad65b2fbcc9dd95027af93ee8237964685c5de80
de3sw2aq1 <de3sw2aq1@ymail.com>
2018-08-02 18:16:25 -0500

    cityofroses: cleanup spider

--- scraper/sites/cityofroses.py
+++ scraper/sites/cityofroses.py
@@ -1,4 +1,3 @@
-import sys
 from lxml.html import builder as E
 from . import Spider
 
@@ -11,41 +10,37 @@ class Cityofroses(Spider):
     def parse(self, url):
         doc = self.fetch(START_URL)
 
-        title, = doc.xpath('//meta[@property="og:site_name"]/@content')
-        self.metadata['title'] = title.strip()
+        self.metadata['title'] = 'City of Roses'
         self.metadata['author'] = 'Kip Manley'
 
         contents = doc.get_element_by_id('thecontents')
-
         for link in contents.xpath('h2/a/@href'):
             yield from self._parse_chapter(link)
 
     def _parse_chapter(self, url):
-        self.info(f'Parsing chapter: {url}' )
+        self.info('Parsing chapter: %s', url)
         doc = self.fetch(url)
 
         title, = doc.xpath('//title/text()')
-        title = title.split(' | ')[-1]
-        yield E.H1(title.strip())
+        title = title.split(' | ')[-1].strip()
+        yield E.H1(title)
 
         content = doc.get_element_by_id('content')
 
         # Replace empty <h6> tags with <br>
-        for h6 in content.xpath('//h6'):
-            hr = E.HR()
-            h6.addprevious(hr)
-            h6.getparent().remove(h6)
+        for h6 in content.iter('h6'):
+            h6.addprevious(E.HR())
+            h6.drop_tree()
 
-        for tag in content.iterchildren():
+        for tag in content:
             # Start of section
             if tag.tag == 'h3':
-                section_title = tag.text_content()
-                yield E.H2(section_title.strip())
+                yield E.H2(tag.text_content().strip())
 
             # Section content
             elif tag.get('class') == 'grafset':
                 yield from tag
-            
+
             # End note
             elif tag.get('class') == 'endnote':
                 # Change <p> into a <blockquote>
