0b37de78ee1506bad525cfd8a946ab426844c4b9
de3sw2aq1 <de3sw2aq1@ymail.com>
2018-08-06 00:33:30 -0500

    Add comments to scraper/__init__.py

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -3,7 +3,20 @@ import pkgutil
 from .spider import Spider
 from . import sites
 
-# Manually load all modules inside sites
+
+# A slightly hacky plugin system
+
+# Spider subclasses are defined in a sites submodule. During import this module
+# imports all modules in the sites submodule. This module makes available a
+# dict mapping domains to spiders.
+
+# A quirk of this importing is that the import heirachy is broken a bit. That
+# is, spiders must use:
+#     from . import Spider
+# instead of:
+#     from .. import Spider
+
+# Manually load all modules inside sites submodule
 for _loader, _name, _ in pkgutil.iter_modules(sites.__path__, prefix=__name__+'.'):
     _loader.find_module(_name).load_module(_name)
 
